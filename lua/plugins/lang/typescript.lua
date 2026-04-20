146077748a24241a5496e018ac3884b1888bb766
4thel00z <4thel00z@gmail.com>
2026-04-20 08:15:02 +0200

    feat(lang): add TypeScript-specific plugins
    - Package-info for package.json version management
    - Show/hide package versions inline
    - Update, install, delete packages
    - Uses bun as package manager
    - Keybindings: <leader>n* for npm/package operations

--- lua/plugins/lang/typescript.lua
+++ lua/plugins/lang/typescript.lua
@@ -1,5 +1,51 @@
 -- lua/plugins/lang/typescript.lua
--- TypeScript-specific plugins
--- Will be populated in Task 15
+-- TypeScript-specific plugins: package-info
 
-return {}
+return {
+  -- Package-info (package.json version management)
+  {
+    "vuki656/package-info.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    event = { "BufRead package.json" },
+    config = function()
+      require("package-info").setup({
+        colors = {
+          up_to_date = "#3C4048",
+          outdated = "#d19a66",
+        },
+        icons = {
+          enable = true,
+          style = {
+            up_to_date = "|  ",
+            outdated = "|  ",
+          },
+        },
+        autostart = true,
+        hide_up_to_date = false,
+        hide_unstable_versions = false,
+        package_manager = "bun", -- Use bun as package manager
+      })
+
+      -- Keybindings for package-info (only in package.json)
+      vim.api.nvim_create_autocmd("BufRead", {
+        pattern = "package.json",
+        callback = function()
+          local opts = { buffer = true, silent = true }
+          vim.keymap.set("n", "<leader>ns", require("package-info").show, vim.tbl_extend("force", opts, { desc = "Show package versions" }))
+          vim.keymap.set("n", "<leader>nc", require("package-info").hide, vim.tbl_extend("force", opts, { desc = "Hide package versions" }))
+          vim.keymap.set("n", "<leader>nt", require("package-info").toggle, vim.tbl_extend("force", opts, { desc = "Toggle package versions" }))
+          vim.keymap.set("n", "<leader>nu", require("package-info").update, vim.tbl_extend("force", opts, { desc = "Update package" }))
+          vim.keymap.set("n", "<leader>nd", require("package-info").delete, vim.tbl_extend("force", opts, { desc = "Delete package" }))
+          vim.keymap.set("n", "<leader>ni", require("package-info").install, vim.tbl_extend("force", opts, { desc = "Install package" }))
+          vim.keymap.set("n", "<leader>np", require("package-info").change_version, vim.tbl_extend("force", opts, { desc = "Change package version" }))
+        end,
+      })
+    end,
+  },
+
+  -- NUI (UI component library, required by package-info)
+  {
+    "MunifTanjim/nui.nvim",
+    lazy = true,
+  },
+}
