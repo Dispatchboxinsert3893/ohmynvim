5e71734065659b7a14522f1f26dd9421fb1c0b4b
4thel00z <4thel00z@gmail.com>
2026-04-20 07:53:39 +0200

    feat(plugins): add debugging support
    - Nvim-dap core debugging with breakpoints and stepping
    - Dap-ui for visual debugging interface
    - Dap-virtual-text for inline variable values
    - Mason-nvim-dap for automatic debugger installation
    - Support for Python (debugpy), Rust/C/C++ (codelldb), Go (delve), JS/TS (node-debug2)
    - Keybindings: <leader>d* for debug operations

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,5 +1,187 @@
 -- lua/plugins/debug.lua
--- Debugging support: nvim-dap, dap-ui, etc.
--- Will be populated in Task 11
+-- Debugging support: nvim-dap, dap-ui, dap-virtual-text, mason-nvim-dap
 
-return {}
+return {
+  -- DAP (Debug Adapter Protocol) core
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+    },
+    keys = {
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint" },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Conditional breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+      { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to cursor" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step into" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Step over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "Step out" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run last" },
+      { "<leader>dt", function() require("dapui").toggle() end, desc = "Toggle DAP UI" },
+      { "<leader>dx", function() require("dap").terminate() end, desc = "Terminate" },
+    },
+    config = function()
+      local dap = require("dap")
+
+      -- DAP signs
+      vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticError", linehl = "", numhl = "" })
+      vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "DiagnosticWarn", linehl = "", numhl = "" })
+      vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
+      vim.fn.sign_define("DapStopped", { text = "󰁕", texthl = "DiagnosticHint", linehl = "Visual", numhl = "" })
+      vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
+
+      -- DAP configurations (will be set up by mason-nvim-dap)
+      -- Configurations for specific languages will be added by mason-nvim-dap
+    end,
+  },
+
+  -- DAP UI (visual debugging interface)
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup({
+        icons = { expanded = "", collapsed = "", current_frame = "" },
+        controls = {
+          icons = {
+            pause = "",
+            play = "",
+            step_into = "",
+            step_over = "",
+            step_out = "",
+            step_back = "",
+            run_last = "",
+            terminate = "",
+            disconnect = "",
+          },
+        },
+        layouts = {
+          {
+            elements = {
+              { id = "scopes", size = 0.25 },
+              { id = "breakpoints", size = 0.25 },
+              { id = "stacks", size = 0.25 },
+              { id = "watches", size = 0.25 },
+            },
+            size = 40,
+            position = "left",
+          },
+          {
+            elements = {
+              { id = "repl", size = 0.5 },
+              { id = "console", size = 0.5 },
+            },
+            size = 10,
+            position = "bottom",
+          },
+        },
+        floating = {
+          max_height = nil,
+          max_width = nil,
+          border = "rounded",
+          mappings = {
+            close = { "q", "<Esc>" },
+          },
+        },
+      })
+
+      -- Automatically open/close DAP UI
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+
+  -- DAP virtual text (show variable values inline)
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap" },
+    config = function()
+      require("nvim-dap-virtual-text").setup({
+        enabled = true,
+        enabled_commands = true,
+        highlight_changed_variables = true,
+        highlight_new_as_changed = false,
+        show_stop_reason = true,
+        commented = false,
+        only_first_definition = true,
+        all_references = false,
+        display_callback = function(variable, buf, stackframe, node, options)
+          if options.virt_text_pos == "inline" then
+            return " = " .. variable.value
+          else
+            return variable.name .. " = " .. variable.value
+          end
+        end,
+        virt_text_pos = vim.fn.has("nvim-0.10") == 1 and "inline" or "eol",
+        all_frames = false,
+        virt_lines = false,
+        virt_text_win_col = nil,
+      })
+    end,
+  },
+
+  -- Mason-nvim-dap (automatically install debuggers)
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
+    cmd = { "DapInstall", "DapUninstall" },
+    config = function()
+      require("mason-nvim-dap").setup({
+        -- Automatically install these debuggers
+        ensure_installed = {
+          "python",      -- Python debugger (debugpy)
+          "codelldb",    -- Rust/C/C++ debugger
+          "delve",       -- Go debugger
+          "js",          -- JavaScript/TypeScript debugger (node-debug2)
+        },
+        automatic_installation = true,
+        handlers = {
+          -- Default handler
+          function(config)
+            require("mason-nvim-dap").default_setup(config)
+          end,
+
+          -- Python-specific configuration
+          python = function(config)
+            config.adapters = {
+              type = "executable",
+              command = "python",
+              args = { "-m", "debugpy.adapter" },
+            }
+            config.configurations = {
+              {
+                type = "python",
+                request = "launch",
+                name = "Launch file",
+                program = "${file}",
+                pythonPath = function()
+                  return "python"
+                end,
+              },
+            }
+            require("mason-nvim-dap").default_setup(config)
+          end,
+        },
+      })
+    end,
+  },
+
+  -- nvim-nio (async I/O library, required by dap-ui)
+  {
+    "nvim-neotest/nvim-nio",
+    lazy = true,
+  },
+}
