2d89ada294798d15c289a51f73b88c9764a56be0
4thel00z <4thel00z@gmail.com>
2026-04-20 06:55:01 +0200

    feat(plugins): add editor enhancement plugins
    - Treesitter for syntax highlighting and text objects
    - Autopairs for auto-closing brackets/quotes
    - Surround for manipulating surrounding pairs
    - Comment for toggling comments (gcc/gbc)
    - Which-key for keybinding discovery
    - Todo-comments for highlighting TODOs
    - Editorconfig support
    - Auto-session for workspace persistence

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,283 @@
 -- lua/plugins/editor.lua
 -- Editor enhancement plugins: treesitter, autopairs, surround, comments, etc.
--- Will be populated in Task 7
 
-return {}
+return {
+  -- Treesitter (syntax highlighting, indentation, text objects)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = "BufReadPost",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- Install parsers for these languages
+        ensure_installed = {
+          "typescript",
+          "tsx",
+          "javascript",
+          "python",
+          "go",
+          "rust",
+          "c",
+          "cpp",
+          "lua",
+          "vim",
+          "vimdoc",
+          "html",
+          "css",
+          "json",
+          "yaml",
+          "toml",
+          "xml",
+          "markdown",
+          "markdown_inline",
+          "bash",
+        },
+        auto_install = true,  -- Auto-install missing parsers
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+              ["]c"] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+              ["[c"] = "@class.outer",
+            },
+          },
+        },
+      })
+    end,
+  },
+
+  -- Autopairs (auto-close brackets, quotes, etc.)
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      local autopairs = require("nvim-autopairs")
+      autopairs.setup({
+        check_ts = true,  -- Use treesitter
+        ts_config = {
+          lua = { "string" },
+          javascript = { "template_string" },
+        },
+        disable_filetype = { "TelescopePrompt", "vim" },
+        fast_wrap = {},
+      })
+
+      -- Integration with nvim-cmp (will be configured later)
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp_status, cmp = pcall(require, "cmp")
+      if cmp_status then
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      end
+    end,
+  },
+
+  -- Surround (add/change/delete surrounding pairs)
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Default keymaps:
+        -- ys{motion}{char} - add surround
+        -- cs{old}{new} - change surround
+        -- ds{char} - delete surround
+        -- Visual mode: S{char} - surround selection
+      })
+    end,
+  },
+
+  -- Comment (toggle comments)
+  {
+    "numToStr/Comment.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("Comment").setup({
+        -- Default keymaps:
+        -- gcc - toggle line comment
+        -- gbc - toggle block comment
+        -- gc{motion} - comment motion
+        -- Visual mode: gc - comment selection
+        padding = true,
+        sticky = true,
+        ignore = "^$",  -- Ignore empty lines
+        toggler = {
+          line = "gcc",
+          block = "gbc",
+        },
+        opleader = {
+          line = "gc",
+          block = "gb",
+        },
+        extra = {
+          above = "gcO",
+          below = "gco",
+          eol = "gcA",
+        },
+        mappings = {
+          basic = true,
+          extra = true,
+        },
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+  },
+
+  -- Context commentstring (for JSX/TSX comments)
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    config = function()
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false,
+      })
+    end,
+  },
+
+  -- Which-key (show keybindings popup)
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      local wk = require("which-key")
+      wk.setup({
+        plugins = {
+          marks = true,
+          registers = true,
+          spelling = {
+            enabled = false,
+          },
+          presets = {
+            operators = true,
+            motions = true,
+            text_objects = true,
+            windows = true,
+            nav = true,
+            z = true,
+            g = true,
+          },
+        },
+        window = {
+          border = "rounded",
+          position = "bottom",
+          margin = { 1, 0, 1, 0 },
+          padding = { 2, 2, 2, 2 },
+        },
+        layout = {
+          height = { min = 4, max = 25 },
+          width = { min = 20, max = 50 },
+          spacing = 3,
+          align = "left",
+        },
+        show_help = true,
+        triggers = "auto",
+        timeout = 300,  -- 300ms delay before popup
+      })
+
+      -- Register leader key groups
+      wk.register({
+        ["<leader>f"] = { name = "Find" },
+        ["<leader>g"] = { name = "Grep" },
+        ["<leader>b"] = { name = "Buffers" },
+        ["<leader>l"] = { name = "LSP" },
+        ["<leader>d"] = { name = "Debug" },
+        ["<leader>w"] = { name = "Windows" },
+        ["<leader>x"] = { name = "Trouble" },
+        ["<leader>s"] = { name = "Search" },
+      })
+    end,
+  },
+
+  -- TODO comments (highlight and search todos)
+  {
+    "folke/todo-comments.nvim",
+    event = "BufReadPost",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup({
+        signs = true,
+        keywords = {
+          FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+          TODO = { icon = " ", color = "info" },
+          HACK = { icon = " ", color = "warning" },
+          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+          PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+          TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        },
+        highlight = {
+          before = "",
+          keyword = "wide",
+          after = "fg",
+          pattern = [[.*<(KEYWORDS)\s*:]],
+          comments_only = true,
+          max_line_len = 400,
+          exclude = {},
+        },
+      })
+    end,
+  },
+
+  -- Editorconfig support
+  {
+    "editorconfig/editorconfig-vim",
+    event = "BufReadPre",
+  },
+
+  -- Auto-session (save/restore sessions per directory)
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+    config = function()
+      require("auto-session").setup({
+        log_level = "error",
+        auto_session_enable_last_session = false,
+        auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
+        auto_session_enabled = true,
+        auto_save_enabled = true,
+        auto_restore_enabled = true,
+        auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+        auto_session_use_git_branch = false,
+        bypass_session_save_file_types = { "alpha", "dashboard" },
+      })
+    end,
+  },
+}
