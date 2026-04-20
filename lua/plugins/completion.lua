46071b1bf3e335213c9d5ec43aabf22f82460eff
4thel00z <4thel00z@gmail.com>
2026-04-20 07:03:09 +0200

    feat(plugins): add completion and snippets
    - Nvim-cmp with multiple sources (LSP, buffer, path, cmdline)
    - LuaSnip snippet engine with jsregexp support
    - Friendly-snippets collection
    - LSPkind for VS Code-like icons
    - Tab to confirm, Ctrl+n/p to navigate
    - Ghost text for suggestions

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,5 +1,164 @@
 -- lua/plugins/completion.lua
--- Completion and snippets: nvim-cmp, LuaSnip, etc.
--- Will be populated in Task 9
+-- Completion and snippets: nvim-cmp, LuaSnip, sources
 
-return {}
+return {
+  -- nvim-cmp (completion engine)
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",     -- LSP source
+      "hrsh7th/cmp-buffer",       -- Buffer source
+      "hrsh7th/cmp-path",         -- Path source
+      "hrsh7th/cmp-cmdline",      -- Cmdline source
+      "saadparwaiz1/cmp_luasnip", -- Snippet source
+      "L3MON4D3/LuaSnip",         -- Snippet engine
+      "rafamadriz/friendly-snippets", -- Snippet collection
+      "onsails/lspkind.nvim",     -- VS Code-like pictograms
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
+
+      -- Load VS Code-style snippets from friendly-snippets
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      -- LuaSnip configuration
+      luasnip.config.setup({
+        history = true,
+        updateevents = "TextChanged,TextChangedI",
+        enable_autosnippets = true,
+      })
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          -- Navigate completion menu
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+
+          -- Scroll documentation window
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+          -- Complete
+          ["<C-Space>"] = cmp.mapping.complete(),
+
+          -- Abort completion
+          ["<C-e>"] = cmp.mapping.abort(),
+
+          -- Confirm selection (Enter or Tab)
+          ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm({ select = true })
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          -- Jump to previous snippet placeholder
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp", priority = 1000 },
+          { name = "luasnip", priority = 750 },
+          { name = "buffer", priority = 500 },
+          { name = "path", priority = 250 },
+        }),
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50,
+            ellipsis_char = "...",
+            before = function(entry, vim_item)
+              return vim_item
+            end,
+          }),
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+      })
+
+      -- Use buffer source for `/` and `?` (search)
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      -- Use cmdline & path source for `:` (commands)
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
+  },
+
+  -- LuaSnip (snippet engine)
+  {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    build = "make install_jsregexp",
+    lazy = true,
+  },
+
+  -- Friendly snippets (snippet collection)
+  {
+    "rafamadriz/friendly-snippets",
+    lazy = true,
+  },
+
+  -- LSP kind (VS Code-like icons)
+  {
+    "onsails/lspkind.nvim",
+    lazy = true,
+  },
+
+  -- Completion sources
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-buffer",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-path",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+    lazy = true,
+  },
+  {
+    "saadparwaiz1/cmp_luasnip",
+    lazy = true,
+  },
+}
